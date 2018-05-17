8ab5f52bb6113bc3e40c42dcd9a04f24315c0699
Nitin <tulswani19@gmail.com>
2018-05-17 16:52:52 +0530

    refactored convertStyles method

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const { StyleSheet } = require('react-native');
 const transform = require('css-to-react-native').default;
 
+/**
+ * Convert css style string to an array object 
+ * @param { string } str CSS string
+ */
 function convertStyles(str) {
-  if (str === null || str === undefined) return '';
+  const arrObj = [];
 
-  const styleObj = [];
-
+  // Parse the style string into prop name and value pair
   const parsedString = str.split(';');
 
+  // Store mappings of prop name and value into an array
   parsedString.forEach(function(style) {
     if (typeof style === 'string') {
       // Get prop name and prop value
       const ar = style.split(': ');
 
       if (ar[0] && ar[1]) {
-        styleObj.push([ar[0].trim(), ar[1].trim()]);
+        arrObj.push([ar[0].trim(), ar[1].trim()]);
       }
     }
   });
 
-  return styleObj;
+  return arrObj;
 }
 
 // cssToRN(`color: red; padding: 20px;`) => [74]
@@ -32,11 +36,11 @@ function cssToRN(styles) {
     // Parse the css string into prop name and value pair
     const parsedCSS = convertStyles(styles);
 
-    const rnStyles = Array.isArray(parsedCSS) ? transform(parsedCSS) : {};
+    const rnStyles = transform(parsedCSS) || {};
 
     // Convert styles to React Native
     return [StyleSheet.create({ style: rnStyles }).style];
-  }
+  } else if (typeof styles === 'string' && styles.length === 0) return ''
 }
 
 module.exports = cssToRN;
