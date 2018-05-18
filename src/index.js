40bc6c35f568b9725fd33777529af46aecc9e534
Nitin <tulswani19@gmail.com>
2018-05-18 09:53:06 +0530

    remove unnecessary string check

--- src/index.js
+++ src/index.js
@@ -1,46 +1,47 @@
-const { StyleSheet } = require('react-native');
-const transform = require('css-to-react-native').default;
+const { StyleSheet } = require('react-native')
+const transform = require('css-to-react-native').default
 
 /**
- * Convert css style string to an array object 
+ * Convert css style string to an array object
  * @param { string } str CSS string
  */
 function convertStyles(str) {
-  const arrObj = [];
+	const arrObj = []
 
-  // Parse the style string into prop name and value pair
-  const parsedString = str.split(';');
+	// Parse the style string into prop name and value pair
+	const parsedString = str.split(';')
 
-  // Store mappings of prop name and value into an array
-  parsedString.forEach(function(style) {
-    if (typeof style === 'string') {
-      // Get prop name and prop value
-      const ar = style.split(': ');
+	// Store mappings of prop name and value into an array
+	parsedString.forEach(function(style) {
+		// Get prop name and prop value
+		const ar = style.split(': ')
 
-      if (ar[0] && ar[1]) {
-        arrObj.push([ar[0].trim(), ar[1].trim()]);
-      }
-    }
-  });
+		if (ar[0] && ar[1]) {
+			arrObj.push([ar[0].trim(), ar[1].trim()])
+		}
+	})
 
-  return arrObj;
+	return arrObj
 }
 
-// cssToRN(`color: red; padding: 20px;`) => [74]
+/**
+ * Convert CSS string to React Native
+ */
+// cssToRN(`color: red; padding: 20px;`) => [{ color: 'red', paddingTop: 20, paddingBottom: 20, paddingLeft: 20, paddingRight: 20 }]
 function cssToRN(styles) {
-  if (typeof styles !== 'string') {
-    throw new Error('Expected a valid css string.');
-  }
+	if (typeof styles !== 'string') {
+		throw new Error('Expected a valid css string.')
+	}
 
-  if (typeof styles === 'string' && styles.length > 0) {
-    // Parse the css string into prop name and value pair
-    const parsedCSS = convertStyles(styles);
+	if (typeof styles === 'string' && styles.length > 0) {
+		// Parse the css string into prop name and value pair
+		const parsedCSS = convertStyles(styles)
 
-    const rnStyles = transform(parsedCSS) || {};
+		const rnStyles = transform(parsedCSS) || {}
 
-    // Convert styles to React Native
-    return [StyleSheet.create({ style: rnStyles }).style];
-  } else if (typeof styles === 'string' && styles.length === 0) return ''
+		// Convert styles to React Native
+		return [StyleSheet.create({ style: rnStyles }).style]
+	} else if (typeof styles === 'string' && styles.length === 0) return ''
 }
 
-module.exports = cssToRN;
+module.exports = cssToRN
